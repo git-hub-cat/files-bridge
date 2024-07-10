cdaccebd0c190b08a101690b73cd2960eac13936
xll <674740193@qq.com>
2024-07-10 15:35:00 +0800

    .

--- dist/magic/routers/router.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const KoaRouter = require("koa-router");
-const router = new KoaRouter();
-exports.default = router;
--- dist/utils/cache.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const NodeCache = require("node-cache");
-const appCache = new NodeCache();
-exports.default = appCache;
